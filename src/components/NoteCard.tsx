8de53c28067b359d2bb69fc56c1b22a31425ce65
tothlevente <tothlevente139@gmail.com>
2024-09-03 10:27:49 +0200

    Developments

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -6,7 +6,8 @@ import NotesIcon from "@mui/icons-material/Notes";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
 
-import { Avatar, IconButton } from "@mui/material";
+import { Avatar, Box, IconButton } from "@mui/material";
+import { blue, grey, red } from "@mui/material/colors";
 
 export default function NoteCard({
   title,
@@ -17,9 +18,15 @@ export default function NoteCard({
   return (
     <Card
       sx={{
-        minWidth: 50,
-        maxWidth: 500,
+        display: "flex",
+        width: 500,
         height: 250,
+        bgcolor: grey[200],
+        "@media (max-width: 560px)": {
+          width: "auto",
+          minWidth: 50,
+          height: 300,
+        },
         "@media (max-width: 459px)": {
           height: 300,
         },
@@ -28,66 +35,73 @@ export default function NoteCard({
         },
       }}
     >
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h6"
-          component="div"
-          sx={{ display: "flex" }}
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Typography
+            gutterBottom
+            variant="h6"
+            component="div"
+            sx={{ display: "flex" }}
+          >
+            <Avatar
+              sx={{
+                marginRight: "10px",
+                color: "background.paper",
+              }}
+            >
+              <NotesIcon />
+            </Avatar>
+            {title.length < 30
+              ? title
+              : title.substring(0, 30) + "..."}
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{ marginTop: "12px", color: "text.secondary" }}
+          >
+            {discription.length < 300
+              ? discription
+              : discription.substring(0, 300) + "..."}
+          </Typography>
+        </CardContent>
+        <CardActions
+          sx={{
+            display: "flex",
+            marginBottom: "20px",
+          }}
         >
-          <Avatar
+          <IconButton
+            edge="end"
+            aria-label="edit"
             sx={{
-              marginRight: "10px",
-              color: "background.paper",
+              marginRight: "3px",
+              "&:hover": {
+                color: blue[500],
+              },
+            }}
+            onClick={() => {
+              setShowHandleEdit(true);
             }}
           >
-            <NotesIcon />
-          </Avatar>
-          {title.length < 30 ? title : title.substring(0, 30) + "..."}
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{ marginTop: "12px", color: "text.secondary" }}
-        >
-          {discription.length < 300
-            ? discription
-            : discription.substring(0, 300) + "..."}
-        </Typography>
-      </CardContent>
-      <CardActions
-        sx={{ justifyContent: "flex-end", marginBottom: "20px" }}
-      >
-        <IconButton
-          edge="end"
-          aria-label="edit"
-          sx={{
-            marginRight: "3px",
-            "&:hover": {
-              color: "#2196f3",
-            },
-          }}
-          onClick={() => {
-            setShowHandleEdit(true);
-          }}
-        >
-          <EditNoteIcon />
-        </IconButton>
-        <IconButton
-          edge="end"
-          aria-label="delete"
-          sx={{
-            marginRight: "5px",
-            "&:hover": {
-              color: "#ff5722",
-            },
-          }}
-          onClick={() => {
-            setShowHandleDelte(true);
-          }}
-        >
-          <DeleteIcon />
-        </IconButton>
-      </CardActions>
+            <EditNoteIcon />
+          </IconButton>
+          <IconButton
+            edge="end"
+            aria-label="delete"
+            sx={{
+              marginRight: "5px",
+              "&:hover": {
+                color: red[500],
+              },
+            }}
+            onClick={() => {
+              setShowHandleDelte(true);
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </CardActions>
+      </Box>
     </Card>
   );
 }
